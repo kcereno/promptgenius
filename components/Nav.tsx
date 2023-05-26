bc55b40f06da2e9a201d0fa6e3339bbf7735e50c
Karl Cereno <kagc.dev@gmail.com>
2023-05-26 13:09:52 -0400

    Finish destop nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="text-xl normal-case btn btn-ghost">daisyUI</a>
+        <a className="text-xl normal-case btn btn-ghost">PromptGenius</a>
       </div>
-
       {/* End */}
-      <div className="dropdown dropdown-end">
+      {/* Mobile */}
+      <div className="dropdown dropdown-end lg:hidden">
         <label
           tabIndex={0}
           className="btn btn-ghost btn-square avatar"
@@ -34,13 +34,25 @@ const Nav = () => {
           className="w-32 p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box"
         >
           <li>
-            <a className="justify-between">Add Prompt</a>
+            <a className="justify-between">Add</a>
           </li>
           <li>
-            <a>All Prompts</a>
+            <a>View All</a>
           </li>
           <li>
             <a>Logout</a>
+          </li>
+        </ul>
+      </div>
+      {/* Desktop */}
+      <div className="flex-none hidden lg:block">
+        <ul className="px-1 menu menu-horizontal">
+          <li>
+            <a>Add</a>
+          </li>
+
+          <li>
+            <a>View All</a>
           </li>
         </ul>
       </div>
