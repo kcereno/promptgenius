891d181049c3bf5f3576ef74bda7fd31b98ffc56
Karl Cereno <kagc.dev@gmail.com>
2023-05-27 12:20:43 -0400

    Change metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <h1>hello</h1>
+    <main className="flex flex-col items-center min-h-screen p-24">
+      <div className="">
+        {' '}
+        <h1>Unlocking AI&apos;s Potential</h1>
+      </div>
+      <div className="">I need help INSERT DROPDOWN HERE</div>
+      <div className="B">BROWSE ALL BUTTON</div>
     </main>
   );
 }
