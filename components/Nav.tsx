f7942fd74e62b36ab99d5267ed638a0846aa2a74
Karl Cereno <kagc.dev@gmail.com>
2023-06-01 13:43:17 -0400

    Change Nav UI

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,49 +13,55 @@ const Nav = ({ changeTheme }: Props) => {
     changeTheme();
   };
 
+  const mobileMenu = (
+    <div className="dropdown">
+      <label
+        tabIndex={0}
+        className="btn btn-ghost lg:hidden"
+      >
+        <HamburgerMenuIcon />
+      </label>
+      <ul
+        tabIndex={0}
+        className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">FAQ</a>
+        </li>
+
+        <li>
+          <a href="">View All</a>
+        </li>
+      </ul>
+    </div>
+  );
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        {/* Mobile Dropdown */}
-        <div className="dropdown">
-          <label
-            tabIndex={0}
-            className="btn btn-ghost lg:hidden"
-          >
-            <HamburgerMenuIcon />
-          </label>
-          <ul
-            tabIndex={0}
-            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">FAQ</a>
-            </li>
-
-            <li>
-              <a href="">View All</a>
-            </li>
-          </ul>
-        </div>
-        {/* Desktop */}
+        <Link
+          href="/"
+          className="text-xl normal-case btn btn-ghost"
+        >
+          Prompt<span className="text-primary">Genius</span>
+        </Link>
+        {mobileMenu}
+      </div>
+      <div className="mt-2 navbar-center">
         <div className="hidden lg:block">
           <ul className="px-1 menu menu-horizontal">
             <li>
-              <a>Home</a>
+              <Link href="/prompts">View All</Link>
             </li>
             <li>
               <a>FAQ</a>
             </li>
-            <li>
-              <Link href="/prompts">View All</Link>
-            </li>
           </ul>
         </div>
       </div>
-      <div className="mt-2 navbar-center"></div>
       <div className="navbar-end">
         <button className="hidden btn btn-ghost">FAQ</button>
         <button className="hidden btn btn-ghost">VIEW ALL</button>
